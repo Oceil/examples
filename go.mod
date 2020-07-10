c7983f4dc44b1b7a99b62f8da43dbc50387c2933
Karim Ahmed <kaelhawaty@gmail.com>
2020-07-11 00:29:10 +0200

    all

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/Oceil/examples
+
+go 1.14
+
+require (
+	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b
+	github.com/google/differential-privacy/go v0.0.0-20200707122900-6f408b48171d
+)
---
+++ main.go
@@ -0,0 +1,82 @@
+//
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+// This is a command line utility which helps to run different examples of how dp library can be used.
+// Usage example:
+// bazel run examples_main -- --scenario=CountVisitsPerHour --input_file=data/day_data.csv --non_private_output_file=$(pwd)/out1.csv --private_output_file=$(pwd)/out2.csv
+package main
+
+import (
+
+	log "github.com/golang/glog"
+	"github.com/Oceil/examples/utils"
+)
+
+const (
+	countVisitsPerHourScenarioID  = "CountVisitsPerHour"
+	countVisitsPerDayScenarioID   = "CountVisitsPerDay"
+	sumRevenuePerDayScenarioID    = "SumRevenuePerDay"
+	countVisitsPerCertainDuration = "CountVisitsPerDuration"
+)
+
+func RunExample(scenario, inputFile, nonPrivateResultsOutputFile, privateResultsOutputFile string) {
+	log.Infof("The example was run with arguments: scenario = %d,"+
+		" inputFile = %q, nonPrivateResultsOutputFile = %q, privateResultsOutputFile = %q",
+		scenario,
+		inputFile,
+		nonPrivateResultsOutputFile,
+		privateResultsOutputFile,
+	)
+
+	if scenario == "" {
+		log.Exit("No scenario was chosen")
+	}
+
+	if inputFile == "" {
+		log.Exit("No input file was chosen")
+	}
+
+	if nonPrivateResultsOutputFile == "" {
+		log.Exit("No output file for non-private results was chosen")
+	}
+
+	if privateResultsOutputFile == "" {
+		log.Exit("No output file for private results was chosen")
+	}
+
+	var err error
+	var sc utils.Scenario
+	switch id := scenario; id {
+	case countVisitsPerHourScenarioID:
+		sc = &utils.CountVisitsPerHourScenario{}
+	case countVisitsPerDayScenarioID:
+		sc = &utils.CountVisitsPerDayScenario{}
+	case sumRevenuePerDayScenarioID:
+		sc = &utils.SumRevenuePerDayScenario{}
+	case countVisitsPerCertainDuration:
+		sc = &utils.CountVisitsPerCertainDurationScenario{}
+	default:
+		log.Exitf("There is no scenario with id = %d", id)
+	}
+
+	err = utils.RunScenario(sc, inputFile, nonPrivateResultsOutputFile, privateResultsOutputFile)
+	if err != nil {
+		log.Exitf("Couldn't execute example, err = %v", err)
+	}
+
+	log.Infof("Successfully finished executing the example")
+}
+func main(){}
